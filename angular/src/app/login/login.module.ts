07c3472ddb39aef02b6fef8f040836861aa51947
nirajsakhare0033 <kangthecon0111@gmail.com>
2023-08-29 00:54:52 +0530

    login module created and login,register component created and user.ts created

--- angular/src/app/login/login.module.ts
+++ angular/src/app/login/login.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserloginComponent } from './userlogin/userlogin.component';
+import { UserregisterComponent } from './userregister/userregister.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserloginComponent,
+    UserregisterComponent
+  ],
   imports: [
     CommonModule
   ]
